1f499c01c011007e07e46cbdd60026ca256d7459
Zoltan45 <rderwis@yahoo.com>
2018-03-31 13:57:00 -0400

    Update libretro.c

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -1991,12 +1991,12 @@ void retro_set_input_state(retro_input_state_t cb) { input_state_cb = cb; }
 
 void retro_get_system_info(struct retro_system_info *info)
 {
-   info->library_name = "Genesis Plus GX";
+   info->library_name = "GPGX - Sega CD";
 #ifndef GIT_VERSION
 #define GIT_VERSION ""
 #endif
    info->library_version = "v1.7.4" GIT_VERSION;
-   info->valid_extensions = "mdx|md|smd|gen|bin|cue|iso|chd|sms|gg|sg";
+   info->valid_extensions = "bin|cue|iso|chd";
    info->block_extract = false;
    info->need_fullpath = true;
 }
